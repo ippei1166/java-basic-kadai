827765579a0386f76246ce067b43e01743e0bf5e
ippei1166 <152014027+ippei1166@users.noreply.github.com>
2024-01-30 18:34:05 +0900

    Add files via upload

---
+++ kadai_004/java/Score_Chapter04.java
@@ -0,0 +1,20 @@
+package kadai_004.java;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		System.out.println("Aさんの点数は10点です");
+		System.out.println("Bさんの点数は20点です");
+		System.out.println("Cさんの点数は30点です");
+		System.out.println("Dさんの点数は40点です");
+		System.out.println("Eさんの点数は50点です");
+		System.out.println("Fさんの点数は60点です");
+		System.out.println("Gさんの点数は70点です");
+		System.out.println("Hさんの点数は80点です");
+		System.out.println("Iさんの点数は90点です");
+		System.out.println("Jさんの点数は100点です");
+		System.out.println("テストの平均点は"+(10+20+30+40+50+60+70+80+90+100)/10+"点");
+
+	}
+
+}
