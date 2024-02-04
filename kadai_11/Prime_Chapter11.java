03ba4c6771a82af33c161a8c56100b3e7849a491
ippei1166 <152014027+ippei1166@users.noreply.github.com>
2024-02-04 11:41:40 +0900

    Add files via upload

---
+++ kadai_11/Prime_Chapter11.java
@@ -0,0 +1,35 @@
+package kadai_11;
+
+public class Prime_Chapter11 {
+    public static void main(String[] args) {
+       findPrimes();
+    }
+
+    public static void findPrimes() {
+        int max = 100;
+        boolean[] primeNum = new boolean[max + 1];
+
+        
+        for (int i = 2; i <= max; i++) {
+            primeNum[i] = true;
+        }
+
+        
+        for (int i = 2; i * i <= max; i++) {
+            if (primeNum[i]) {
+                for (int j = i * i; j <= max; j += i) {
+                    primeNum[j] = false; 
+                }
+            }
+        }
+
+        
+        System.out.println();
+        for (int i = 2; i <= max; i++) {
+            if (primeNum[i]) {
+
+                System.out.println(i);
+            }
+        }
+    }
+}
